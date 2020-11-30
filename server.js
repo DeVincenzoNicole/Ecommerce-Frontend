64ed018425930c383299d38972b1e8a1d13c6cb2
DeVincenzoNicole <devincenzo.nicole@hotmail.com>
2020-11-30 12:03:55 -0300

    deployment

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const express = require('express');
+
+const app = express();
+
 app.use(express.static('./dist/angular-ecommerce'));
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/angular-ecommerce/'}
-  );
-  });
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-ecommerce/'}),
+);
+
 
 app.listen(process.env.PORT || 8080);
