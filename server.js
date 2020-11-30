3867631fbc7792f235dd81c5d9b356e538bd176a
DeVincenzoNicole <devincenzo.nicole@hotmail.com>
2020-11-30 14:31:41 -0300

    modificaciones para el deploy

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname+'/dist/angular-ecommerce'));
+app.use(express.static('./dist/angular-ecommerce'));
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname+'/dist/angular-ecommerce/index.html'));
-  });
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', { root: 'dist/angular-ecommerce' }
+  );
+});
 
 app.listen(process.env.PORT || 8080);
