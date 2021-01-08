c5be4fb4b0a6d5c8a02e3ba48d34305e5f70b5df
DeVincenzoNicole <64378602+DeVincenzoNicole@users.noreply.github.com>
2021-01-08 13:46:03 -0300

    Update product.service.ts

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -12,9 +12,9 @@ import { ProductCategory } from '../common/product-category';
 export class ProductService {
 
 
-  private baseUrl = 'http://localhost:8080/api/products';
+  private baseUrl = 'https://springboot-ecommerce-nicole.herokuapp.com/api/products';
 
-  private categoryUrl = 'http://localhost:8080/api/product-category';
+  private categoryUrl = 'https://springboot-ecommerce-nicole.herokuapp.com/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
